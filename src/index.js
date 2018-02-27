19b06847bcec8afd8fb02212c4943e4ae65d16b0
heyunjiang <598119677@qq.com>
2018-02-27 22:09:23 +0800

    成功区分生产环境和调试环境配置

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack2234466'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,7 @@
+ const path = require('path');
  const merge = require('webpack-merge');
  const common = require('./webpack.common.js');
+ const webpack = require('webpack');
 
  module.exports = merge(common, {
  	devtool: 'inline-source-map',
@@ -8,5 +10,9 @@
  		compress: true,
  		port: 9000,
  		hot: true
- 	}
+ 	},
+ 	plugins: common.plugins.concat([
+ 		new webpack.NamedModulesPlugin(),
+    	new webpack.HotModuleReplacementPlugin()
+ 	])
  });
